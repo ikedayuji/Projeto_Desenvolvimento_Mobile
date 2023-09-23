d56a36f4de8e21fac5e151e7e0d976ccc2c7319f
Eric <ikedayuji.2002@gmail.com>
2023-09-22 21:52:26 -0300

    Atividade V1 22/09/2023

--- lib/home.dart
+++ lib/home.dart
@@ -14,59 +14,46 @@ class HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    Timer(
-      const Duration(seconds: 3),
-      () {
-        Navigator.pop(context);
-        Navigator.push(
-          context, 
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pop(context);
+      Navigator.push(
+          context,
           MaterialPageRoute(
-            //builder: (context) => TopBar()
-            builder: (context) => BottomBar()
-          )
-        );
-      }
-    );
+              //builder: (context) => TopBar()
+              builder: (context) => BottomBar()));
+    });
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          color: Colors.black87,
-          gradient: LinearGradient(
-            colors: [
-              Colors.deepPurple,
-              Colors.blueAccent,
-              Colors.cyanAccent
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-          ),
-          /*RadialGradient(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                color: Colors.black87,
+                gradient: LinearGradient(colors: [
+                  Colors.lightGreen,
+                  Colors.redAccent,
+                  Colors.yellowAccent,
+                ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                /*RadialGradient(
             colors: [
               Colors.yellowAccent,
               Colors.yellow,
               Colors.red
             ]
           ),*/
-          // Imagem de fundo
-          image: DecorationImage(
-            image: AssetImage("./assets/images/fundo.jpg")
-          )
-        ),
-        /* Se estiver executando na Web (Chrome) */
-        //child: Image.asset("/images/computer.png")
-        /* Se estiver executando no Emulador Android */
-        /*child: Image.asset(
+                // Imagem de fundo
+                image: DecorationImage(
+                    image: AssetImage("./assets/images/fundo.jpg"))),
+            /* Se estiver executando na Web (Chrome) */
+            //child: Image.asset("/images/computer.png")
+            /* Se estiver executando no Emulador Android */
+            /*child: Image.asset(
           "./assets/images/computer.png"
         )*/
-        child: Lottie.asset("./assets/images/animacao.json")
-      )
-    );
+            child: Lottie.asset("./assets/images/Animacao_teste2.json")));
   }
 }
